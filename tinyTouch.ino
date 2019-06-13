27d79307570009a000b12bb6b30eaf6bda2069b5
Afroditi Psarra <stereochic@gmail.com>
2019-06-12 18:01:51 -0700

    Update tinyTouch.ino

--- tinyTouch.ino
+++ tinyTouch.ino
@@ -2,11 +2,12 @@
  *
  * Circuit: 
  * ATtiny85 
- * Capacitive sensors on pins 2,3,4
- * 3 x 4.7 MΩ resistors between pins 1 and 2,3,4
+ * Capacitive sensors on pins 3,4,1
+ * 3 x 1.5 MΩ resistors between pins 2 and 3,4,1
  * Speaker-sound output on pin 0 (PWM pin for further developement of the code)
  *
  * April-May 2014
+ * Revised on June 2019
  */
 
 // include the Arduino CapSense library
@@ -27,7 +28,7 @@ int duration;
 // capacitive sensors
 CapacitiveSensor yellow = CapacitiveSensor(2,3);        // 4.7 MΩ resistor between pins 2 and 3, pin 3 is the sensor pin
 CapacitiveSensor orange = CapacitiveSensor(2,4);        // 4.7 MΩ resistor between pins 2 and 4, pin 4 is the sensor pin
-CapacitiveSensor green = CapacitiveSensor(2,1);        // 4.7 MΩ resistor between pins 2 ans 2, pin 1 is the sensor pin
+CapacitiveSensor green = CapacitiveSensor(2,1);        // 4.7 MΩ resistor between pins 2 and 1, pin 1 is the sensor pin
 
 // variables that hold the sensors' readings
 long yellowVal;
